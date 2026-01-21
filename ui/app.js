2ec26a926caaedde90c588f5c380cf409db3787b
Matthew Geier <matthew.geier.mg@gmail.com>
2026-01-20 22:42:33 -0500

    Use relative API URL for Render

--- ui/app.js
+++ ui/app.js
@@ -15,7 +15,7 @@ document.getElementById("generateBtn").addEventListener("click", async () => {
     }
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/api/flashcards", {
+        const response = await fetch("/api/flashcards", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
